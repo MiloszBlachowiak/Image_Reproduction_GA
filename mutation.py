8bc986c0d6e2754190fdfc6a45f8b9e6fa860009
keliasz <KEliasz145@gmail.com>
2023-03-19 18:47:11 +0100

    random change - mutation

--- mutation.py
+++ mutation.py
@@ -1,4 +1,5 @@
 # https://sound.eti.pg.gda.pl/student/isd/isd03-algorytmy_genetyczne.pdf
+import numpy as np
 
 # short description
 def mutationName():
@@ -8,8 +9,14 @@ def replacement():
     raise NotImplementedError
 
 
-def random_swap():
-    raise NotImplementedError
+def random_change(population, mutation_percent):
+    for idx in range(population.shape[0]):
+        
+        rand_idx = np.uint32(np.random.random(size=np.uint32(mutation_percent/100*population.shape[1]))
+                                                    *population.shape[1])
+        new_values = np.uint8(np.random.random(size=rand_idx.shape[0])*256)
+        population[idx, rand_idx] = new_values
+    return population
 
 
 def adjacent_swap():
