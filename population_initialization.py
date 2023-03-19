1f01ee1d108b5256fa670b674efd633a85958570
agnieszkakojs <72501306+agnieszkakojs@users.noreply.github.com>
2023-03-19 09:38:45 +0100

    Update population_initialization.py

--- population_initialization.py
+++ population_initialization.py
@@ -1,8 +1,16 @@
 # https://medium.datadriveninvestor.com/population-initialization-in-genetic-algorithms-ddb037da6773
 
-def random():
-    raise NotImplementedError
+import numpy as np
+import random 
 
+def random(img_size, number_individuals=10):
+    size_chromosom = img_size[0]*img_size[1]*3
+    # Creating an empty list to hold individuals
+    population = np.empty(shape=(number_individuals, size_chromosom), dtype=np.uint8)
+    # Filling individuals with random data
+    for indv_num in range(number_individuals):
+         population[indv_num] = np.random.random(size_chromosom)*256
+    return population
 
 def heuristic():
     raise NotImplementedError
