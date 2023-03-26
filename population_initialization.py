b79b7bd0bc5db6d5402ec9b3c99b4992e6f2adb2
agnieszkakojs <agakojs@student.agh.edu.pl>
2023-03-26 11:36:39 +0200

    refactor population initialization

--- population_initialization.py
+++ population_initialization.py
@@ -10,11 +10,14 @@ def random(img_size, number_individuals=10):
     else:
       # For color images
       size_chromosom = img_size[0]*img_size[1]*3
+      
     # Creating an empty list to hold individuals
     population = np.empty(shape=(number_individuals, size_chromosom), dtype=np.uint8)
+    
     # Filling individuals with random data
     for indv_num in range(number_individuals):
          population[indv_num] = np.random.random(size_chromosom)*256
+         
     return population
 
 def heuristic():
