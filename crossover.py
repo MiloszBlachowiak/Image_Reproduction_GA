9344b809bfb1c02d986b91f7a5321193f5559166
MiloszBlachowiak <mblachowiak@student.agh.edu.pl>
2023-03-26 16:51:08 +0200

    Added uniform crossover method

--- crossover.py
+++ crossover.py
@@ -2,9 +2,9 @@
 import numpy as np
 from itertools import combinations
 from math import ceil
-   
 
-# single operation of single-point crossover    
+
+# single operation of single-point crossover
 def single_point(parent_one: np.ndarray, parent_two: np.ndarray):
 
     size = parent_one.size
@@ -30,8 +30,26 @@ def two_point(parent_one: np.ndarray, parent_two: np.ndarray):
     return offspring_one, offspring_two
 
 
-def uniform():
-    raise NotImplementedError
+# single operation of uniform crossover
+def uniform(parent_one: np.ndarray, parent_two: np.ndarray):
+    size = parent_one.size
+
+    # randomly picking half the genes' ids
+    genes = np.random.choice(range(size), int(size/2), replace=False)
+    genes = sorted(genes)
+
+    offspring_one = np.zeros((size))
+    offspring_two = np.zeros((size))
+
+    for i in range(size):
+        if i in genes:
+            offspring_one[i] = parent_one[i]
+            offspring_two[i] = parent_two[i]
+        else:
+            offspring_one[i] = parent_two[i]
+            offspring_two[i] = parent_one[i]
+
+    return offspring_one, offspring_two
 
 
 # function performing crossover operation over a given mating pool
@@ -43,7 +61,7 @@ def perform_crossover(mating_pool: np.ndarray, method: str="single_point", numbe
 
     # randomly choosing uniqe pairs of parents' ids to cross over
     chosen_pairs_ids = np.random.choice(len(parents_pairs_permutations), ceil(number_of_offsprings / 2), replace=False)
-    
+
     for pair_id in chosen_pairs_ids:
         parents_pair = parents_pairs_permutations[pair_id]
 
@@ -51,13 +69,13 @@ def perform_crossover(mating_pool: np.ndarray, method: str="single_point", numbe
             offspring_one, offspring_two = single_point(mating_pool[parents_pair[0]], mating_pool[parents_pair[1]])
         elif method == "two_point":
             offspring_one, offspring_two = two_point(mating_pool[parents_pair[0]], mating_pool[parents_pair[1]])
-    
+
         offsprings.append(offspring_one)
         offsprings.append(offspring_two)
 
-    # since every single crossover operations gives us 2 offsprings
+    # since every single crossover operation gives us 2 offsprings
     # if the given number of offsprings is odd, we have to remove random offspring from the list of offsprings
     if number_of_offsprings % 2 == 1:
         offsprings.pop(np.random.randint(len(offsprings)))
-    
+
     return np.array(offsprings)
