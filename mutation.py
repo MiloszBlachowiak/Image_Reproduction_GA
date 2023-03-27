887b4b4e12d2bf606a9ccb4ae8b6ae88aea2303a
keliasz <KEliasz145@gmail.com>
2023-03-27 14:59:43 +0200

    added other mutations

--- mutation.py
+++ mutation.py
@@ -1,17 +1,9 @@
 # https://sound.eti.pg.gda.pl/student/isd/isd03-algorytmy_genetyczne.pdf
 import numpy as np
-
-# short description
-def mutationName():
-    raise NotImplementedError
     
-def replacement():
-    raise NotImplementedError
 
-
-def random_change(population, mutation_percent):
+def replacement(population: np.ndarray, mutation_percent: float) -> np.ndarray:
     for idx in range(population.shape[0]):
-        
         rand_idx = np.uint32(np.random.random(size=np.uint32(mutation_percent/100*population.shape[1]))
                                                     *population.shape[1])
         new_values = np.uint8(np.random.random(size=rand_idx.shape[0])*256)
@@ -19,14 +11,40 @@ def random_change(population, mutation_percent):
     return population
 
 
-def adjacent_swap():
-    raise NotImplementedError
+def random_swap(population: np.ndarray, mutation_percent: float) -> np.ndarray:
+    for idx in range(population.shape[0]):
+        rand_idx_1 = np.uint32(np.random.random(size=np.uint32(mutation_percent/100*population.shape[1]))
+                                                    *population.shape[1])
+        rand_idx_2 = np.uint32(np.random.random(size=np.uint32(mutation_percent/100*population.shape[1]))
+                                                    *population.shape[1])
+        population[idx, rand_idx_1], population[idx, rand_idx_2] = population[idx, rand_idx_2], population[idx, rand_idx_1]
+    return population
 
 
-def end_for_end_swap():
-    raise NotImplementedError
+def adjacent_swap(population: np.ndarray, mutation_percent: float) -> np.ndarray:
+    for idx in range(population.shape[0]):
+        rand_idx_1 = np.uint32(np.random.random(size=np.uint32(mutation_percent/100*population.shape[1]))
+                                                    *population.shape[1])
+        if np.any(rand_idx_1 == population.shape[1]-1):
+            ind = np.where(rand_idx_1 == population.shape[1]-1)
+            rand_idx_1 = np.delete(rand_idx_1, ind)
+        idx_2 = rand_idx_1 + 1
+        population[idx, rand_idx_1], population[idx, idx_2] = population[idx, idx_2], population[idx, rand_idx_1]
+    return population
 
 
-def inversion():
-    raise NotImplementedError
+def end_for_end_swap(population: np.ndarray, mutation_percent: float) -> np.ndarray:
+    for idx in range(population.shape[0]):
+        if np.random.random(size=1)[0] <= mutation_percent:
+            rand_idx = np.uint32(np.random.random(size=1) * population.shape[1])[0]
+            population[idx] = np.array(list(population[idx, rand_idx:]) + list(population[idx, :rand_idx]))
+    return population
 
+
+def inversion(population: np.ndarray, mutation_percent: float) -> np.ndarray:
+    for idx in range(population.shape[0]):
+        if np.random.random(size=1)[0] <= mutation_percent:
+            rand_idx = np.uint32(np.random.random(size=2) * population.shape[1])
+            first, second = min(rand_idx), max(rand_idx)
+            population[idx, first:second] = np.array(list(population[idx, first:second])[::-1])
+    return population
